12b47c5365d056fdc0234669771c11c216e0dbce
Modgo <abdelaldev@gmail.com>
2025-02-01 12:26:19 +0100

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request,jsonify
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify('Server is Up')
+
+@app.route('/download')
+def download():
+    download_url = request.args.get('url')
+    return render_template('download.html', download_url=download_url)
+
 
 
 if __name__ == '__main__':
